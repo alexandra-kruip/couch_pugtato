f4b8c45f0ea45a36eb8dd23740bd5ea9143be763
alexsjlee <sjlee89@gmail.com>
2017-07-18 12:36:05 -0700

    Added basic footer

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './app.css';
+import Footer from './footer';
 
 const App = () => (
     <div>
         <div className="app">
             <h1>Welcome to Couch Pugtato!</h1>
         </div>
+        <Footer />
     </div>
 );
 
