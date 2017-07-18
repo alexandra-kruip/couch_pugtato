5355895802bb5916fd66fadf3598291eb1c61f3e
apellanr <ryan.apellanes@gmail.com>
2017-07-18 13:53:27 -0700

    bootstrap navbar comp added

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './app.css';
+import Navbar from './navbar';
 
 const App = () => (
     <div>
+        <Navbar />
         <div className="app">
             <h1>Welcome to Couch Pugtato!</h1>
         </div>
