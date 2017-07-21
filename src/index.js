a4c1e8908e4db071e178e9bc4de1e384755e4b5e
alexsjlee <sjlee89@gmail.com>
2017-07-21 16:11:50 -0700

    Revert "add pugtato in img"

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
-import rootReducer from './reducers/index';
 
 import App from './components/app';
 
-const createStoreWithMiddleware = applyMiddleware(promise, thunk)(createStore);
-
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(rootReducer)}>
-        <App />
-    </Provider>,
+    <App />,
     document.getElementById('root')
 );
