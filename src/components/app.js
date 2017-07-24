069d5926342e96e34dd855a6d2465c9337e263d2
alexsjlee <sjlee89@gmail.com>
2017-07-24 11:26:26 -0700

    Added modal to youtube trailers

--- src/components/app.js
+++ src/components/app.js
@@ -1,32 +1,20 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import './app.css';
 import Navbar from './navbar';
 import Footer from './footer';
 import Grid from './grid';
 import VideoList from './videolist';
 
-class App extends Component {
-    render() {
-        console.log('from app', this.props.youtubeBoolean)
-        return(
-            <div>
-                <Navbar /> 
-                <div className="app">
-                    <h1>Welcome to Couch Pugtato!</h1>
-                </div>
-                <Grid />
-                <VideoList show={!this.props.youtubeBoolean} />
-                <Footer /> 
-            </div>
-        )
-    }
-};
+const App = () => (
+    <div>
+        <Navbar /> 
+        <div className="app">
+            <h1>Welcome to Couch Pugtato!</h1>
+        </div>
+        <Grid />
+        <VideoList />
+        <Footer /> 
+    </div>
+);
 
-function mapStateToProps(state) {
-    return{
-        youtubeBoolean: state.youtubeBoolean.youtubeBoolean,
-    }
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
